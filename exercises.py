18bcb3a32706d948b5d5fafb5fbb9291f77488b5
julian torres <juliannogueira@yahoo.com>
2018-05-10 20:20:06 -1000

    added another item to position (2)

--- exercises.py
+++ exercises.py
@@ -11,7 +11,8 @@ print('testing')
 #2. You just created a networking study list and forget to add in 'SSH'. Please add that into the 3rd position in the networking list and print your results.
 
 networking = ['packet', 'LAN', 'WAN', 'port', 'firewall', 'VPN']
-
+networking.insert(2, 'SSH')
+print(networking)
 
 #remove(element) removes a single element from the list
 #3. The cyber security analyst entered the wrong IP address in the list below. Please remove the non-float integer from the ip addy list and print your results.
